85c762a95147a4e89740bc6a4b6b4c1f40be2b65
GauChoob <starlight_tonight@hotmail.com>
2025-02-09 14:44:46 -0500

    Load lev and lgr simultaneously, not sequentially

--- src/controller.js
+++ src/controller.js
@@ -57,253 +57,277 @@ export default function(_args) {
     var canvas = canvase.getContext("2d");
     elem.appendChild(canvase);
 
-    getString(levelUrl, function(lev) {
-      var levRd = levReader(lev)
-      const setup = function(lgrFile) {
-        var pllgr = new LGRWrapper(lgrFile, legacy_url);
-        var pl = player(levRd, pllgr, mkCanv, autoPlay);
-
-        function listener(e) {
-          if (pl.inputKey(e.key)) e.preventDefault();
-        }
-
-        canvase.setAttribute("tabindex", "0");
-        canvase.addEventListener("keydown", listener, true);
-        var play = true;
-        var animationLoop;
-        var loop =
-          typeof requestAnimationFrame != "undefined"
-            ? function(fn) {
-                void (function go() {
-                  fn();
-                  play && requestAnimationFrame(go);
-                })();
-              }
-            : function(fn) {
-                var fps = 30;
-                animationLoop = setInterval(fn, 1000 / fps);
-              };
-
-        function draw() {
-          var status = pl.draw(canvas, 0, 0, canvase.width, canvase.height, true);
-          status && onFrameUpdate(status.currentFrame, status.maxFrames);
-        }
-        loop(draw);
-
-        function rect() {
-          return canvase.getBoundingClientRect();
-        }
-
-        canvase.addEventListener("click", function(e) {
-          var r = rect();
-          pl.inputClick(
-            e.clientX - r.left,
-            e.clientY - r.top,
-            canvase.width,
-            canvase.height
-          );
-          e.preventDefault();
-        });
-
-        canvase.addEventListener("mousedown", function(e) {
-          var r = rect();
-          var cont = pl.inputDrag(
-            e.clientX - r.left,
-            e.clientY - r.top,
-            canvase.width,
-            canvase.height
-          );
-
-          function onmousemove(e) {
-            cont.update(e.clientX - r.left, e.clientY - r.top);
-            e.preventDefault();
-          }
-
-          function onmouseup() {
-            cont.end();
-            // /me dislikes function identity
-            document.removeEventListener("mousemove", onmousemove);
-            document.removeEventListener("mouseup", onmouseup);
-            e.preventDefault();
-          }
-
-          document.addEventListener("mousemove", onmousemove);
-          document.addEventListener("mouseup", onmouseup);
-        });
-
-        canvase.addEventListener("touchstart", function ontouchstart(e) {
-          var ts = e.changedTouches;
-          var r = rect();
-
-          if (ts.length < 1) return;
-          e.preventDefault();
-
-          var cont = pl.inputDrag(
-            ts[0].clientX - r.left,
-            ts[0].clientY - r.top,
-            canvase.width,
-            canvase.height
-          );
-
-          var isClick = true;
-
-          function ontouchmove(e) {
-            var ts = e.changedTouches;
-            if (ts.length < 1) return;
-            isClick = false;
-            cont.update(ts[0].clientX - r.left, ts[0].clientY - r.top);
-            e.preventDefault();
-          }
-
-          function ontouchend() {
-            cont.end();
-            if (isClick)
-              pl.inputClick(
-                ts[0].clientX - r.left,
-                ts[0].clientY - r.top,
-                canvase.width,
-                canvase.height
-              );
-            // ..
-            document.removeEventListener("touchmove", ontouchmove);
-            document.removeEventListener("touchend", ontouchend);
-            document.removeEventListener("touchcancel", ontouchend);
-
-            canvase.addEventListener("touchstart", ontouchstart);
-          }
-
-          document.addEventListener("touchmove", ontouchmove);
-          document.addEventListener("touchend", ontouchend);
-          document.addEventListener("touchcancel", ontouchend);
-
-          canvase.removeEventListener("touchstart", ontouchstart);
-        });
-
-        canvase.addEventListener("wheel", function(e) {
-          var r = rect();
-          var delta =
-            e.deltaMode == WheelEvent.DOM_DELTA_LINE
-              ? (53 / 3) * e.deltaY
-              : e.deltaY;
-          pl.inputWheel(
-            e.clientX - r.left,
-            e.clientY - r.top,
-            canvase.width,
-            canvase.height,
-            delta
-          );
-          e.preventDefault();
-        });
-
-        cont({
-          loadReplay: function(recName, shirts) {
-            getString(recName, function(rec) {
-              if (rec) {
-                pl.addReplay(
-                  recReader(rec),
-                  !shirts
-                    ? []
-                    : shirts.map(function(s) {
-                        return s == null ? null : new UrlImage(pllgr, '', s);
-                      })
-                );
-              }
-            });
-          },
-
-          loadReplays: function(recNames, shirts) {
-            let loadedShirt = !shirts
-              ? []
-              : shirts.map(function(s) {
-                  return s == null ? null : new UrlImage(pllgr, '', s);
-                });
-
-            recNames.map(function(r) {
-              return getString(r, function(rec) {
-                pl.addReplay(recReader(rec), r, [loadedShirt[0]]);
-                loadedShirt = loadedShirt.slice(1);
-              });
-            });
-          },
-
-          changeReplays: function(recNames, shirts) {
-            const loadedRecs = pl.loadedRecs();
-
-            loadedRecs.forEach(function(r) {
-              if (!recNames.includes(r)) {
-                pl.removeReplay(r);
-              }
-            });
-
-            const newRecs = [];
-            const newShirts = [];
-
-            recNames.forEach(function(r, i) {
-              if (loadedRecs.includes(r)) {
-                return;
-              }
-
-              newRecs.push(r);
-              newShirts.push(shirts[i]);
-            });
-
-            let loadedShirt = newShirts.map(function(s) {
-              return s == null ? null : new UrlImage(pllgr, '', s);
-            });
-
-            newRecs.map(function(r) {
-              return getString(r, function(rec) {
-                pl.addReplay(recReader(rec), r, [loadedShirt[0]]);
-                loadedShirt = loadedShirt.slice(1);
-              });
-            });
-          },
-
-          removeAnimationLoop: function() {
-            play = false;
-            animationLoop && window.clearInterval(animationLoop);
-          },
-
-          loadLevel: function(levelUrl, cont) {
-            getString(levelUrl, function(lev) {
-              pl.changeLevel(levReader(lev));
-              if (cont) cont();
-            });
-          },
-
-          resize: function(wd, ht) {
-            canvase.width = wd;
-            canvase.height = ht;
-            pl.invalidate();
-          },
-
-          setFrame: function(frame) {
-            pl.setFrame(frame);
-          },
-
-          player: function() {
-            return pl;
-          },
-        });
+    // Try to download the level and lgr at the same time (unless we need to get the lgr name from the level file)
+    var pllgr = null;
+    var levRd = null;
+    const assetLoaded = () => {
+      if (pllgr && levRd) {
+        setup();
       }
-      const getBackupLgr = (originalUrl) => () => {
-        // Try to download default.lgr, or else use legacy png lgr
-        console.log(`Recplayer - Unable to load ${originalUrl}, trying to load ${defaultLgrUrl} instead`)
-        getArray(defaultLgrUrl, setup, () => {
-          console.log(`Recplayer - Unable to load ${defaultLgrUrl}, using legacy .png files`)
-          setup(null)
-        })
-      }
-      if (lgrFrom === 'legacy') {
-        setup(null);
-      }
-      if (lgrFrom === 'file') {
-        getArray(lgrUrl, setup, getBackupLgr(lgrUrl));
-      }
-      if (lgrFrom === 'level') {
+      if (levRd && !pllgr && lgrFrom === 'level') {
         var lgrUrlComplete = `${lgrUrl}${levRd.lgr()}`;
-        getArray(lgrUrlComplete, setup, getBackupLgr(lgrUrlComplete));
+        getLgrFile(lgrUrlComplete)
       }
+    }
+
+    getString(levelUrl, function(lev) {
+      levRd = levReader(lev)
+      assetLoaded();
     });
+
+    const getBackupLgr = (originalUrl) => () => {
+      // If we are unsuccessful at downloading the main lgr, try to download default.lgr, or else use legacy png lgr
+      console.log(`Recplayer - Unable to load ${originalUrl}, trying to load ${defaultLgrUrl} instead`)
+      getArray(defaultLgrUrl, lgrFile => {
+        pllgr = new LGRWrapper(lgrFile, undefined);
+        assetLoaded();
+      }, () => {
+        console.log(`Recplayer - Unable to load ${defaultLgrUrl}, using legacy .png files`)
+        legacy_getLgr();
+      })
+    }
+    const legacy_getLgr = () => {
+      pllgr = new LGRWrapper(null, legacy_url)
+      assetLoaded();
+    }
+    const getLgrFile = (url) => {
+      getArray(url, lgrFile => {
+        pllgr = new LGRWrapper(lgrFile, undefined);
+        assetLoaded();
+      }, getBackupLgr(url));
+    }
+    if (lgrFrom === 'legacy') {
+      legacy_getLgr();
+    }
+    if (lgrFrom === 'file') {
+      getLgrFile(lgrUrl);
+    }
+
+    const setup = function() {
+      var pl = player(levRd, pllgr, mkCanv, autoPlay);
+
+      function listener(e) {
+        if (pl.inputKey(e.key)) e.preventDefault();
+      }
+
+      canvase.setAttribute("tabindex", "0");
+      canvase.addEventListener("keydown", listener, true);
+      var play = true;
+      var animationLoop;
+      var loop =
+        typeof requestAnimationFrame != "undefined"
+          ? function(fn) {
+              void (function go() {
+                fn();
+                play && requestAnimationFrame(go);
+              })();
+            }
+          : function(fn) {
+              var fps = 30;
+              animationLoop = setInterval(fn, 1000 / fps);
+            };
+
+      function draw() {
+        var status = pl.draw(canvas, 0, 0, canvase.width, canvase.height, true);
+        status && onFrameUpdate(status.currentFrame, status.maxFrames);
+      }
+      loop(draw);
+
+      function rect() {
+        return canvase.getBoundingClientRect();
+      }
+
+      canvase.addEventListener("click", function(e) {
+        var r = rect();
+        pl.inputClick(
+          e.clientX - r.left,
+          e.clientY - r.top,
+          canvase.width,
+          canvase.height
+        );
+        e.preventDefault();
+      });
+
+      canvase.addEventListener("mousedown", function(e) {
+        var r = rect();
+        var cont = pl.inputDrag(
+          e.clientX - r.left,
+          e.clientY - r.top,
+          canvase.width,
+          canvase.height
+        );
+
+        function onmousemove(e) {
+          cont.update(e.clientX - r.left, e.clientY - r.top);
+          e.preventDefault();
+        }
+
+        function onmouseup() {
+          cont.end();
+          // /me dislikes function identity
+          document.removeEventListener("mousemove", onmousemove);
+          document.removeEventListener("mouseup", onmouseup);
+          e.preventDefault();
+        }
+
+        document.addEventListener("mousemove", onmousemove);
+        document.addEventListener("mouseup", onmouseup);
+      });
+
+      canvase.addEventListener("touchstart", function ontouchstart(e) {
+        var ts = e.changedTouches;
+        var r = rect();
+
+        if (ts.length < 1) return;
+        e.preventDefault();
+
+        var cont = pl.inputDrag(
+          ts[0].clientX - r.left,
+          ts[0].clientY - r.top,
+          canvase.width,
+          canvase.height
+        );
+
+        var isClick = true;
+
+        function ontouchmove(e) {
+          var ts = e.changedTouches;
+          if (ts.length < 1) return;
+          isClick = false;
+          cont.update(ts[0].clientX - r.left, ts[0].clientY - r.top);
+          e.preventDefault();
+        }
+
+        function ontouchend() {
+          cont.end();
+          if (isClick)
+            pl.inputClick(
+              ts[0].clientX - r.left,
+              ts[0].clientY - r.top,
+              canvase.width,
+              canvase.height
+            );
+          // ..
+          document.removeEventListener("touchmove", ontouchmove);
+          document.removeEventListener("touchend", ontouchend);
+          document.removeEventListener("touchcancel", ontouchend);
+
+          canvase.addEventListener("touchstart", ontouchstart);
+        }
+
+        document.addEventListener("touchmove", ontouchmove);
+        document.addEventListener("touchend", ontouchend);
+        document.addEventListener("touchcancel", ontouchend);
+
+        canvase.removeEventListener("touchstart", ontouchstart);
+      });
+
+      canvase.addEventListener("wheel", function(e) {
+        var r = rect();
+        var delta =
+          e.deltaMode == WheelEvent.DOM_DELTA_LINE
+            ? (53 / 3) * e.deltaY
+            : e.deltaY;
+        pl.inputWheel(
+          e.clientX - r.left,
+          e.clientY - r.top,
+          canvase.width,
+          canvase.height,
+          delta
+        );
+        e.preventDefault();
+      });
+
+      cont({
+        loadReplay: function(recName, shirts) {
+          getString(recName, function(rec) {
+            if (rec) {
+              pl.addReplay(
+                recReader(rec),
+                !shirts
+                  ? []
+                  : shirts.map(function(s) {
+                      return s == null ? null : new UrlImage(pllgr, '', s);
+                    })
+              );
+            }
+          });
+        },
+
+        loadReplays: function(recNames, shirts) {
+          let loadedShirt = !shirts
+            ? []
+            : shirts.map(function(s) {
+                return s == null ? null : new UrlImage(pllgr, '', s);
+              });
+
+          recNames.map(function(r) {
+            return getString(r, function(rec) {
+              pl.addReplay(recReader(rec), r, [loadedShirt[0]]);
+              loadedShirt = loadedShirt.slice(1);
+            });
+          });
+        },
+
+        changeReplays: function(recNames, shirts) {
+          const loadedRecs = pl.loadedRecs();
+
+          loadedRecs.forEach(function(r) {
+            if (!recNames.includes(r)) {
+              pl.removeReplay(r);
+            }
+          });
+
+          const newRecs = [];
+          const newShirts = [];
+
+          recNames.forEach(function(r, i) {
+            if (loadedRecs.includes(r)) {
+              return;
+            }
+
+            newRecs.push(r);
+            newShirts.push(shirts[i]);
+          });
+
+          let loadedShirt = newShirts.map(function(s) {
+            return s == null ? null : new UrlImage(pllgr, '', s);
+          });
+
+          newRecs.map(function(r) {
+            return getString(r, function(rec) {
+              pl.addReplay(recReader(rec), r, [loadedShirt[0]]);
+              loadedShirt = loadedShirt.slice(1);
+            });
+          });
+        },
+
+        removeAnimationLoop: function() {
+          play = false;
+          animationLoop && window.clearInterval(animationLoop);
+        },
+
+        loadLevel: function(levelUrl, cont) {
+          getString(levelUrl, function(lev) {
+            pl.changeLevel(levReader(lev));
+            if (cont) cont();
+          });
+        },
+
+        resize: function(wd, ht) {
+          canvase.width = wd;
+          canvase.height = ht;
+          pl.invalidate();
+        },
+
+        setFrame: function(frame) {
+          pl.setFrame(frame);
+        },
+
+        player: function() {
+          return pl;
+        },
+      });
+    }
   };
 }
